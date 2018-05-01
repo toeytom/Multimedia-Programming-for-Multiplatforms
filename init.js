75e8b02c13b58c8cb0765157058d167d643301c4
toeytom <toeytom2551@hotmail.com>
2018-05-01 13:36:35 +0700

    add kahang

---
+++ init.js
@@ -0,0 +1,7 @@
+var scene
+var kahang
+function init (){
+    scene = new Scene();
+    scene.start()
+    kahang = new Sprite(scene,"1.jpg",50,50)
+}
---
+++ update.js
@@ -0,0 +1,4 @@
+function update(){
+    scene.clear()
+    kahang.update()
+}
